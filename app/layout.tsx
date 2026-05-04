ffff91edf08c1f59d5192590dfde6505276f125c
João Rocha <joao.santos@unitri.edu.br>
2026-05-04 16:12:33 -0300

    coonfigurações da netlify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter, Space_Grotesk } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import CustomHead from './head'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
@@ -18,7 +17,6 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-sans`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
